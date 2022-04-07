5be53725bf52b85606ac00870b6ce1d45eeb45e9
phorgegiving <kocharga200@gmail.com>
2022-04-07 19:58:44 +0300

    fixed button

--- java/claimbutton.js
+++ java/claimbutton.js
@@ -3,12 +3,10 @@
     document.querySelector("button").onclick = function() {
         number = number + 0.05;
         document.getElementById("number").innerHTML = 'Your balance is ' + Math.floor(number * 100) / 100 + ' ART';
+        document.querySelector("button").setAttribute('disabled', 'disabled');
     }
 
         function btnenabl() {
         document.getElementById('rewa').removeAttribute("disabled");}
         
-        document.querySelector("button").onclick = function() {
-            document.querySelector("button").setAttribute('disabled', 'disabled');
-          }
         
