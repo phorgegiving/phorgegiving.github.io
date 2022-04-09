75057b02b5862319f578aae546e7632293624f1a
phorgegiving <kocharga200@gmail.com>
2022-04-09 16:03:00 +0300

    Update claimbutton.js

--- java/claimbutton.js
+++ java/claimbutton.js
@@ -4,6 +4,7 @@
         number = number + 0.05;
         document.getElementById("number").innerHTML = 'Your balance is ' + Math.floor(number * 100) / 100 + ' ART';
         document.querySelector("button").setAttribute('disabled', 'disabled');
+        grecaptcha.reset();
     }
 
         function btnenabl() {
