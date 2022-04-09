db82301f4e165661a03b543758cdc3852e152b37
phorgegiving <kocharga200@gmail.com>
2022-04-09 17:01:25 +0300

    уфкт рсфзесрф

--- java/claimbutton.js
+++ java/claimbutton.js
@@ -4,7 +4,7 @@
         number = number + 0.05;
         document.getElementById("number").innerHTML = 'Your balance is ' + Math.floor(number * 100) / 100 + ' ART';
         document.querySelector("button").setAttribute('disabled', 'disabled');
-        grecaptcha.reset();
+        hcaptcha.reset();
         localStorage.setItem('number', number);  
     }
 
